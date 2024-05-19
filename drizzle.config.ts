36240ececfe402b7f46ef58b7856ca212a952497
Oumar Barry <oumar.barry1202@gmail.com>
2024-05-19 02:41:59 +0000

    chore: revamp

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,8 @@ import type { Config } from "drizzle-kit"
 export default {
   schema: "./src/db/schema.ts",
   out: "./migrations",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: process.env.DATABASE_URL,
-  },
+  dialect: "postgresql",
+  dbCredentials: { url: process.env.DATABASE_URL },
   verbose: true,
   strict: true,
 } satisfies Config
